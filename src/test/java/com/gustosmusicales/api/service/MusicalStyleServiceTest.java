a6cc3dc34df79ae13d273557aba897eac46282e9
Nicolás Herrera <josnikh@gmail.com>
2022-07-05 11:52:47 -0500

    indications for apiKey header

--- src/test/java/com/gustosmusicales/api/service/MusicalStyleServiceTest.java
+++ src/test/java/com/gustosmusicales/api/service/MusicalStyleServiceTest.java
@@ -3,20 +3,28 @@ package com.gustosmusicales.api.service;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Order;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.MockitoAnnotations;
 
+import java.util.ArrayList;
 import java.util.List;
 import static org.junit.Assert.assertEquals;
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.when;
 import com.gustosmusicales.api.repository.MusicalStyleRepository;
 import com.gustosmusicales.api.models.MusicalStyle;
 import com.gustosmusicales.api.dto.*;
 import com.gustosmusicales.api.service.impl.MusicalStyleServiceImpl;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+import static org.mockito.Mockito.*;
+import org.junit.Before;
+import static org.mockito.ArgumentMatchers.any;
+
+
+@ExtendWith(MockitoExtension.class)
 public class MusicalStyleServiceTest {
 	
 	@Mock
@@ -24,21 +32,31 @@ public class MusicalStyleServiceTest {
 
     @InjectMocks
     private MusicalStyleServiceImpl musicalStyleServiceImpl;
+
+    
+    @Before
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
     
     @Order(1) 
     @DisplayName("JUnit test service para guardar un estilo musical")
     @Test
     public void saveMusicalStyle() {
-    	MusicalStyleDto musicalStyleDto = new MusicalStyleDto("Pop");
-    	MusicalStyle savedMusicalStyle = musicalStyleServiceImpl.add(musicalStyleDto);
-        assertThat(savedMusicalStyle).isNotNull();
-        assertThat(savedMusicalStyle.getId()).isGreaterThan(0);
+    	MusicalStyle style = new MusicalStyle(1L, "Vallenato");
+        when(musicalStyleRepository.save(any(MusicalStyle.class))).thenReturn(style);
+    	MusicalStyle savedMusicalStyle = musicalStyleServiceImpl.add(new MusicalStyleDto("Vallenato"));
+    	assertNotNull(savedMusicalStyle.getName());
     }
     
     @Order(2) 
     @DisplayName("JUnit test service para listar los estilos musicales")
     @Test
     public void getListMusicalStyle() {
+    	List<MusicalStyle> musicalStyleList = new ArrayList<MusicalStyle>();
+    	musicalStyleList.add(new MusicalStyle((long)1, "Vallenato"));;
+        doReturn(musicalStyleList).when(musicalStyleRepository).findAll();
+    	
     	List<MusicalStyle> result = musicalStyleServiceImpl.getAll();
         assertEquals(1, result.size());
     }
@@ -46,8 +64,11 @@ public class MusicalStyleServiceTest {
     @Order(3) 
     @DisplayName("JUnit test service para listar los totales de los estilos musicales")
     @Test
-    
     public void getListTotalMusicalStyle() {
+    	List<MusicalStyle> musicalStyleList = new ArrayList<MusicalStyle>();
+    	musicalStyleList.add(new MusicalStyle((long)1, "Vallenato"));;
+        doReturn(musicalStyleList).when(musicalStyleRepository).findAllWithTotal();
+        
     	List<MusicalStyleScoreDto> result = musicalStyleServiceImpl.findAllWithTotal();
         assertEquals(1, result.size());
     }
